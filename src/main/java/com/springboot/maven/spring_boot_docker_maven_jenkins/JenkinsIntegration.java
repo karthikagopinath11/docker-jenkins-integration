1f2ec275e3fdb17f7ce5216d81eb174b93c60ada
karthikagopinath11 <karthikagopinath14@gmail.com>
2025-07-03 17:12:51 +0530

    Second Commit

---
+++ src/main/java/com/springboot/maven/spring_boot_docker_maven_jenkins/JenkinsIntegration.java
@@ -0,0 +1,9 @@
+package com.springboot.maven.spring_boot_docker_maven_jenkins;
+
+public class JenkinsIntegration{
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Hello");
+	}
+}

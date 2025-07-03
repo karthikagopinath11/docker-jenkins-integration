a1f3b41c942a40582ed1ef320de335934ea40b52
karthikagopinath11 <karthikagopinath14@gmail.com>
2025-07-03 15:03:53 +0530

    first commit

---
+++ src/main/java/com/springboot/maven/spring_boot_docker_maven_jenkins/SpringBootDockerMavenJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.maven.spring_boot_docker_maven_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMavenJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerMavenJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/maven/spring_boot_docker_maven_jenkins/SpringBootDockerMavenJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.maven.spring_boot_docker_maven_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavenJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
